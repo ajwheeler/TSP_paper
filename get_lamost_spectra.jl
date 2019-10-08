2af6f06efa77ead7a1caebf3bc347b96c4006371
Adam Wheeler <adamwhlr@gmail.com>
2019-10-08 09:47:04 -0400

    get lamost spectra

--- get_lamost_spectra.jl
+++ get_lamost_spectra.jl
@@ -3,14 +3,12 @@ using JLD2
 using FileIO
 using Interpolations
 
-
-function download_spectra(obsids::Vector, dir="downloaded_spectra")
+function download_lamost_spectra(obsids::Vector, dir="LAMOST_spectra")
     fns = readdir(dir)
     downloaded_obsids = Set([parse(Int, split(fn, '?')[1])
                              for fn in fns if fn[end-6:end] == "?token="])
     s = sum([o in downloaded_obsids for o in obsids])
     println("of $(length(obsids)) spectra, $(length(obsids) - s) need to be downloaded")
-
     for obsid in obsids
         if !(obsid in downloaded_obsids)
             url = "http://dr4.lamost.org/./spectrum/fits/$(obsid)?token="
@@ -19,12 +17,13 @@ function download_spectra(obsids::Vector, dir="downloaded_spectra")
     end
 end
 
-function load_spectrum(obsid::Integer; dir="downloaded_spectra", interpolate=true, normalize=true)
+function load_lamost_spectrum(obsid::Integer; dir="LAMOST_spectra", 
+                              wl_grid=load("wl_grid.jld2")["wl_grid"], L=50)
     hdu = FITS("$(dir)/$(obsid)?token=")[1]
     header = read_header(hdu)
     data = read(hdu)
     wl = data[:, 3] .- data[:, 3].*header["Z"]
-    if interpolate
+    if wl_grid != nothing
         wl_grid = load("wl_grid.jld2")["wl_grid"]
         flux = (LinearInterpolation(wl, data[:, 1])).(wl_grid)
         ivar = (LinearInterpolation(wl, data[:, 2])).(wl_grid)
@@ -33,13 +32,25 @@ function load_spectrum(obsid::Integer; dir="downloaded_spectra", interpolate=tru
         flux = data[:, 1]
         ivar = data[:, 2]
     end
-    if !normalize
+    if L == 0
         return wl, flux, ivar
     end
 
-    continuum = calculate_continuum(wl, flux, ivar)
+    continuum = calculate_continuum(wl, flux, ivar, L=L)
     flux ./= continuum
     ivar .*= continuum.^2
 
-    return wl, flux, ivar
+    return Float32.(wl), Float32.(flux), Float32.(ivar)
+end
+
+function smoothing_kernel(wl, wl0, L) 
+    exp(- (((wl-wl0)/(L^2))^2) / 2)
+end
+function calculate_continuum(wls, fluxes, ivars; L=50, window=3L)
+    map(zip(wls, fluxes, ivars)) do triple
+        wl, flux, ivar = triple
+        indices = wl-window .< wls .< wl+window
+        weights = smoothing_kernel.(wls[indices], wl, L) .* ivars[indices]
+        sum(weights .* fluxes[indices])/sum(weights)
+    end
 end
--- model.jl
+++ model.jl
@@ -1,11 +1,19 @@
 using LinearAlgebra, Optim
 
+spectral_dist(f1, s1, f2, s2) = sum(@. (f1-f2)^2/(s1^2 + s2^2))
+
 """
 - `F` is the (npix x K) flux matrix
-- `S` is a (npix x K) matrix containing the uncertainties on F
+- `S` is a (npix x K) matrix containing the uncertainties on F.
+   If it is not supplied F is assumed to be exact.
 - `f` is the spectrum under investigation
 - `σ` is the vector of uncertainties for `f`
 """
+function calculate_weights(F::Matrix{Fl}, f::Vector{Fl}, 
+                           σ::Vector{Fl}) where Fl <: AbstractFloat
+    invΣ = inv(Diagonal(σ))
+    (transpose(F) * invΣ * F) \ (transpose(F)  * invΣ * f)
+end
 function calculate_weights(F::Matrix{Fl}, S::Matrix{Fl}, 
                            f::Vector{Fl}, σ::Vector{Fl}) where Fl <: AbstractFloat
     @assert (npix = length(f)) == length(σ) == size(F, 1) == size(S, 1)
@@ -22,9 +30,7 @@ function calculate_weights(F::Matrix{Fl}, S::Matrix{Fl},
     end
 
     #start optimizer at weights you get if F has no error
-    invΣ = inv(Diag(σ))
-    w_init = (transpose(F) * invΣ * F) \ (transpose(F)  * invΣ * f)
-
+    w_init = calculate_weights(F, f, σ)
     res = optimize(objective, w_init, Newton(), autodiff=:forward)
     res.minimizer
 end
